89cc97714fcfe582e812366887765de6623d9034
objayemmy <opeobajuluwa4@gmail.com>
2024-02-01 15:40:00 -0800

    confirmed

---
+++ queue.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+
+/**
+ * f_queue - Switches to queue mode
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_queue(stack_t **head, unsigned int counter)
+{
+    (void)head;
+    (void)counter;
+    bus.lifi = 1;
+}
+
+/**
+ * addqueue - Adds a node to the tail of the stack
+ * @head: head of the stack
+ * @n: new_value
+ * Return: no return
+ */
+void addqueue(stack_t **head, int n)
+{
+    stack_t *new_node, *aux;
+
+    aux = *head;
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: Unable to allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = n;
+    new_node->next = NULL;
+
+    if (aux)
+    {
+        while (aux->next)
+            aux = aux->next;
+    }
+
+    if (!aux)
+    {
+        *head = new_node;
+        new_node->prev = NULL;
+    }
+    else
+    {
+        aux->next = new_node;
+        new_node->prev = aux;
+    }
+}
